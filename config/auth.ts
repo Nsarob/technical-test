3ae09c586c2e4e5ce45a5e17d1b5d4c785775c8b
nsarob <roboticsdamour447@gmail.com>
2025-12-04 09:51:28 +0200

    fix: add trustHost to NextAuth config for Netlify deployment

--- config/auth.ts
+++ config/auth.ts
@@ -6,6 +6,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authConfig: NextAuthConfig = {
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [
     CredentialsProvider({
       name: "Email",
